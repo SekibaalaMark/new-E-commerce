f541b49de92d0b9e4a38baf556fa6eb4b0beb6ea
sekibaalamark <markmajje256@gmail.com>
2026-03-21 18:26:54 +0300

    i have removed the insecure secret django key

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-()*_!hr#j37_hdipac*+7383-6-p&3(3hxhjlpwgclt9o2umjz'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
