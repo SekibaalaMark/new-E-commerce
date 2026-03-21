da15607713236bea373c57e142c6c145eb551031
sekibaalamark <markmajje256@gmail.com>
2026-03-21 16:08:29 +0300

    configured my manage.py file to check the DEBUG value to determine the settings to run

--- backend/manage.py
+++ backend/manage.py
@@ -3,10 +3,20 @@
 import os
 import sys
 
+from backend.settings import base
+
+
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.base')
+    if base.DEBUG:
+        
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.local')
+    
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.production')
+        
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
