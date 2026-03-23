e7b30e8a80a2bc2a903ca10cf47f691be05a62f9
sekibaalamark <markmajje256@gmail.com>
2026-03-23 15:42:56 +0300

    updated the category model to use nested categories

--- backend/backend/product/models.py
+++ backend/backend/product/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+from mptt.models import MPTTModel , TreeForeignKey
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    parent = TreeForeignKey("self",on_delete=models.PROTECT,null=True,blank=True)
 
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
 
 
-
-    
