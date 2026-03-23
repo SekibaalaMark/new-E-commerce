c3d48ff378a7bc6970579363b1afa31c47bd60e5
sekibaalamark <markmajje256@gmail.com>
2026-03-23 15:50:54 +0300

    created the product model

--- backend/backend/product/models.py
+++ backend/backend/product/models.py
@@ -4,9 +4,27 @@ from mptt.models import MPTTModel , TreeForeignKey
 class Category(models.Model):
     name = models.CharField(max_length=100)
     parent = TreeForeignKey("self",on_delete=models.PROTECT,null=True,blank=True)
+    
+    class MPTTMeta:
+        order_insertion_by = ['name']    
+    def __str__(self):
+        return self.name
 
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
 
 
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextChoices(blank=True)
+    is_digital = models.BooleanField(default=False)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE) 
+    category = TreeForeignKey("Category",null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
+    
